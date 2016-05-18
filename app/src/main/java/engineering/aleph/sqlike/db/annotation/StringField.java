d877ff693af55d3c0d5f21ed264031220dc8e771
yarieldis <yarieldis@gmail.com>
2016-05-18 12:14:09 -0400

    Annotations to use on entities and fields.

---
+++ app/src/main/java/engineering/aleph/sqlike/db/annotation/StringField.java
@@ -0,0 +1,12 @@
+package engineering.aleph.sqlike.db.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface StringField {
+}
