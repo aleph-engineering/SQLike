28eb9390323eec3dfc35b5731c08d479b421f700
yarieldis <yarieldis@gmail.com>
2016-05-18 12:17:22 -0400

    Helper class for utils features.

---
+++ app/src/main/java/engineering/aleph/sqlike/db/base/Helper.java
@@ -0,0 +1,37 @@
+package engineering.aleph.sqlike.db.base;
+
+
+import android.util.Log;
+
+import java.lang.reflect.Type;
+
+public class Helper {
+
+    private static final String TAG = "Helper";
+
+    public static boolean extendsOf(Type pType, Class<?> pClass) {
+        boolean returnedValue = false;
+        try {
+            if (pType == pClass) {
+                returnedValue = true;
+            } else {
+                Class<?> superClass = ((Class<?>)pType).getSuperclass();
+                while(superClass != null && superClass != Object.class) {
+                    if (superClass == pClass) {
+                        returnedValue = true;
+                        break;
+                    }
+                    superClass = superClass.getSuperclass();
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getLocalizedMessage());
+        }
+        return returnedValue;
+    }
+
+    public static String capitalize(String pValue) {
+        if (pValue.length() <= 1) return pValue;
+        return pValue.substring(0, 1).toUpperCase() + pValue.substring(1);
+    }
+}
